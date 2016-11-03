a169e77eabb8dd5aa59ca7c03bb042c29ed36f45
rajesh <rajeshn597@gmail.com>
2016-11-03 08:56:41 +0530

    initial commit

---
+++ voteRZR/voteRZR/voteRZR.h
@@ -0,0 +1,19 @@
+//
+//  voteRZR.h
+//  voteRZR
+//
+//  Created by Rajeshnath Chyarngayil Vishwanath on 11/3/16.
+//  Copyright © 2016 Rajeshnath Chyarngayil Vishwanath. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for voteRZR.
+FOUNDATION_EXPORT double voteRZRVersionNumber;
+
+//! Project version string for voteRZR.
+FOUNDATION_EXPORT const unsigned char voteRZRVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <voteRZR/PublicHeader.h>
+
+
